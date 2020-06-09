ef40dc88c1dc0eb9eae4d6ed9793eafa26f523f7
leihailong <leihailong@xueleyun.com>
2020-06-09 20:59:02 +0800

    en

--- mysql/certificate.sql
+++ mysql/certificate.sql
@@ -35,9 +35,10 @@ CREATE TABLE common_school
     icon                    VARCHAR(64)  COMMENT '学校校徽',
     school_address          VARCHAR(512) COMMENT '学校的详细地址',
     school_birthday         date         COMMENT '校庆日',
+    school_tile             VARCHAR(512) COMMENT '学校的各种tile(如：985,211,双一流)',
     section                 smallint(4)  COMMENT '学校段位(1：专科，2：普通本科，3：985、211，4：研究生)',
     certificate_hot         int(11)      COMMENT '学校的火热程度(0-100的值)',
-    branch_schoolid_list    VARCHAR(64)  COMMENT '当前分校的分校id(学校ID之间用,隔开，如：s2417,s8268)',
+    branch_schoolid_list    VARCHAR(64)  COMMENT '当前学校的分校id(学校ID之间用,隔开，如：s2417,s8268)',
     update_time             TIMESTAMP    COMMENT '更新字段的时间戳',
     PRIMARY KEY (id),
     KEY idx (province_id,city_id,school_id)
@@ -53,12 +54,13 @@ DROP TABLE if exists common_postgraduate_profession;
 CREATE TABLE common_postgraduate_profession
 (
     id                      BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-    school_id               VARCHAR(8)  COMMENT '学校ID(s开头的5位，如：s2417)',
+    school_id               VARCHAR(8)   COMMENT '学校ID(s开头的5位，如：s2417)',
     school_name             VARCHAR(64)  COMMENT '学校名称',
     icon                    VARCHAR(64)  COMMENT '学校校徽',
+    school_tile             VARCHAR(512) COMMENT '专业的各种tile(如：985,211,双一流)',
     profession_id           VARCHAR(16)  COMMENT '专业ID(p开头的7位数，如：p241745)',
     profession_name         VARCHAR(64)  COMMENT '专业name',
-    profession_tile         VARCHAR(512) COMMENT '专业的各种tile(如：985,211,双一流)',
+    profession_section      smallint(4)  COMMENT '专业段位(1：专硕，2：学硕)',
     school_order            int(11)      COMMENT '学校综合实力在全国学校中的排名',
     profession_order        int(11)      COMMENT '专业的综合实力在全国同专业中的排名',
     last_5_admission_score  VARCHAR(32)  COMMENT '近五年的录取分数线(如：412,423,400,418,420)',
