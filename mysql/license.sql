ee54c8d8f565d28c135e3b6d6a17c10c4336393c
leihailong <leihailong@xueleyun.com>
2020-06-10 11:05:10 +0800

    en

--- mysql/license.sql
+++ mysql/license.sql
@@ -2,20 +2,40 @@
 
 
 
-#  证书类型维度表
-DROP TABLE if exists common_certificate;
+#  证书维度表
+DROP TABLE if exists common_license;
 CREATE TABLE common_certificate
 (
     id                      BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-    certificate_id          VARCHAR(64)  COMMENT '证书ID',
-    certificate_name        VARCHAR(64)  COMMENT '证书名称',
-    certificate_sort        smallint(4)  COMMENT '证书分类(待定，还没有做出分类)',
-    certificate_hot         int(11)      COMMENT '学生考这个证书的火热程度(0-100的值)',
+    license_id              VARCHAR(16)  COMMENT '证书ID(c开头的7位数，如：c241745)',
+    license_name            VARCHAR(64)  COMMENT '证书名称',
+    license_sort            smallint(4)  COMMENT '证书分类(待定，还没有做出分类)',
+    hot                     int(8)       COMMENT '学生考这个证书的火热程度(0-100的值)',
     update_time             TIMESTAMP    COMMENT '更新字段的时间戳',
     PRIMARY KEY (id),
     KEY idx (certificate_id)
 )ENGINE=INNODB DEFAULT CHARSET=utf8
 COMMENT='证书维度表'
+
+
+
+
+
+#  专业维度表
+DROP TABLE if exists common_profession;
+CREATE TABLE common_profession
+(
+    id                      BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    profession_id           VARCHAR(16)  COMMENT '专业ID(p开头的7位数，如：p241745)',
+    profession_name         VARCHAR(64)  COMMENT '专业name',
+    profession_section      smallint(4)  COMMENT '专业段位(1：专科专业，2：本科专业，7：专硕专业，8：学硕专业)',
+    update_time             TIMESTAMP    COMMENT '更新字段的时间戳',
+    PRIMARY KEY (id),
+    KEY idx (profession_id)
+)ENGINE=INNODB DEFAULT CHARSET=utf8
+COMMENT='专业维度表'
+#  说明：如果是专科专业，就是p1开头的7位数，如果是本科专业，就是p2开头，如果是专硕专业，就是p5，如果是学硕专业，就是p6
+
 
 
 
@@ -30,15 +50,15 @@ CREATE TABLE common_school
     province_name           VARCHAR(64)  COMMENT '省份name',
     city_id                 VARCHAR(8)   COMMENT '地市ID',
     city_name               VARCHAR(64)  COMMENT '地市name',
-    school_id               VARCHAR(8)   COMMENT '学校ID(s开头的5位，如：s2417)',
+    school_id               VARCHAR(8)   COMMENT '学校ID(s开头的5位，如：s24175)',
     school_name             VARCHAR(64)  COMMENT '学校名称',
     icon                    VARCHAR(64)  COMMENT '学校校徽',
     school_address          VARCHAR(512) COMMENT '学校的详细地址',
     school_birthday         date         COMMENT '校庆日',
     school_tile             VARCHAR(512) COMMENT '学校的各种tile(如：985,211,双一流)',
-    section                 smallint(4)  COMMENT '学校段位(1：专科，2：普通本科，3：985、211，4：研究生)',
-    certificate_hot         int(11)      COMMENT '学校的火热程度(0-100的值)',
-    branch_schoolid_list    VARCHAR(64)  COMMENT '当前学校的分校id(学校ID之间用,隔开，如：s2417,s8268)',
+    section                 smallint(4)  COMMENT '学校段位(1：专科，2：普通本科，3：985、211，7：研究生院校或研究院)',
+    hot                     int(8)       COMMENT '学校的火热程度(0-100的值)',
+    branch_schoolid_list    VARCHAR(64)  COMMENT '当前学校的分校id(学校ID之间用,隔开，如：s24172,s82684)',
     update_time             TIMESTAMP    COMMENT '更新字段的时间戳',
     PRIMARY KEY (id),
     KEY idx (province_id,city_id,school_id)
@@ -60,7 +80,8 @@ CREATE TABLE common_postgraduate_profession
     school_tile             VARCHAR(512) COMMENT '专业的各种tile(如：985,211,双一流)',
     profession_id           VARCHAR(16)  COMMENT '专业ID(p开头的7位数，如：p241745)',
     profession_name         VARCHAR(64)  COMMENT '专业name',
-    profession_section      smallint(4)  COMMENT '专业段位(1：专硕，2：学硕)',
+    profession_section      smallint(4)  COMMENT '专业段位(7：专硕，8：学硕)',
+    hot                     int(8)       COMMENT '学生考这个专业的火热程度(0-100的值)',
     school_order            int(11)      COMMENT '学校综合实力在全国学校中的排名',
     profession_order        int(11)      COMMENT '专业的综合实力在全国同专业中的排名',
     last_5_admission_score  VARCHAR(32)  COMMENT '近五年的录取分数线(如：412,423,400,418,420)',
@@ -69,6 +90,12 @@ CREATE TABLE common_postgraduate_profession
     KEY idx (school_id,profession_id)
 )ENGINE=INNODB DEFAULT CHARSET=utf8
 COMMENT='研究生专业维度表'
+
+
+
+
+
+
 
 
 
--- mysql/m_users.sql
+++ mysql/m_users.sql
@@ -10,7 +10,7 @@ CREATE TABLE m_users
     nick_name               VARCHAR(64)  COMMENT '用户昵称',
     sex                     smallint(4)  COMMENT '性别(0：女，1：男)',
     icon                    VARCHAR(64)  COMMENT '用户头像',
-    school_section          smallint(4)  COMMENT '学段(0：大学生，1：研究生)',
+    school_section          smallint(4)  COMMENT '学段(1：专科专业，2：本科专业，7：专硕专业，8：学硕专业)',
     school_id               VARCHAR(8)   COMMENT '学校ID',
     school_name             VARCHAR(64)  COMMENT '学校name',
     profession_id           VARCHAR(16)  COMMENT '专业ID(p开头的7位数，如：p241745)',
@@ -23,9 +23,37 @@ CREATE TABLE m_users
     PRIMARY KEY (id),
     KEY idx (user_id)
 )ENGINE=INNODB DEFAULT CHARSET=utf8
-COMMENT='用户表'
+COMMENT='用户基本表'
 
 
 #     说明：
-#       用户的user_id，是hashkeys
+#       用户的user_id，是手机号、appid等第三方登陆标识
 #
+
+
+
+
+
+
+
+# 用户证书认证表
+DROP TABLE if exists m_certification;
+CREATE TABLE m_certification
+(
+    id                      BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    user_id                 VARCHAR(64)  COMMENT '用户ID',
+    school_section          smallint(4)  COMMENT '学段(1：大学生，7：研究生)',
+    school_id               VARCHAR(8)   COMMENT '学校ID',
+    school_name             VARCHAR(64)  COMMENT '学校name',
+    certificate_id          VARCHAR(16)  COMMENT '证书ID(包含研究生专业id、和考的专业证书id)',
+    certificate_name        VARCHAR(16)  COMMENT '证书name(包含研究生专业名，证书名称)',
+    type                    smallint(4)  COMMENT '证书类型(0：学生考的专业证书，1：专科学校学生证，2：本科学校学生证，7：专硕专业学生证，8：学硕专业学生证)',
+    photo                   VARCHAR(64)  COMMENT '用户用于认证的证书照片',
+    is_authenticate         smallint(4)  COMMENT '认证是否通过(0：未认证，1：已认证)',
+    status                  smallint(4)  COMMENT '用户状态(-1：账户已注销，0：已经毕业，1：正常)',
+    create_time             TIMESTAMP    COMMENT '用户注册时间的时间戳',
+    update_time             TIMESTAMP    COMMENT '用户更新字段的时间戳',
+    PRIMARY KEY (id),
+    KEY idx (user_id)
+)ENGINE=INNODB DEFAULT CHARSET=utf8
+COMMENT='用户基本表'
